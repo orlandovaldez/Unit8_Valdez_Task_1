eb2cf12990366992af70677ec7adc1feb2a8bcf6
orlandovaldez <orlando.valdez@my.tccd.edu>
2020-02-27 01:44:33 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+Orlando Valdez
+COSC 1436
+2-26-2020
+repl.it 
+*/
+
+#include <iostream>
+
+using namespace std;
+
+struct car
+{
+  string Make;
+  string Model;
+  int Year;
+
+
+};
+
+
+
+
+int main()
+{
+
+  return 0;
+}
